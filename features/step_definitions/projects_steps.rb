889d3a87ced080d15323b58126567e0960889a54
Basab K. Maulik <bkm.hadoop@gmail.com>
2012-01-20 22:20:40 -0800

    Consolidated step definitions for Projects in the project_steps.rb file.

--- features/step_definitions/projects_steps.rb
+++ features/step_definitions/projects_steps.rb
@@ -1,3 +1,9 @@
+require 'uri'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+
+Given /^there is a project called "([^"]*)"$/ do |name|
+  @project = Factory(:project, name: name)
+end
 
 Given /^(?:|I )am on the (.+) page$/ do |page_name|
   visit path_to(page_name.downcase!)
@@ -9,6 +15,10 @@ end
 
 When /^I fill in the "([^"]*)" field with "([^"]*)"$/ do |field_name, value|
   fill_in(field_name, with: value)
+end
+
+When /^I fill in "([^"]*)" with "([^"]*)"$/ do |field, value|
+  fill_in(field, with: value)
 end
 
 When /^I click the "([^"]*)" button$/ do |button|
@@ -24,6 +34,23 @@ Then /^I should see the "([^"]*)" message$/ do |text|
     page.should have_content(text)
   else
     assert page.has_content?(text)
+  end
+end
+
+Then /^I should see the message "([^"]*)"$/ do |text|
+  if page.respond_to? :should
+    page.should have_content(text)
+  else
+    assert page.has_content?(text)
+  end
+end
+
+Then /^I should be on the "([^"]*)" project page$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
   end
 end
 
