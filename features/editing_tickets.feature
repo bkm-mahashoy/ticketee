39b427f96837b9d6a5d270f24964d6c7e5950db3
Basab K. Maulik <bkm.hadoop@gmail.com>
2012-02-02 01:37:57 -0800

    Implemented feature to restrict the editing and updating of tickets for a project to users with permission.

--- features/editing_tickets.feature
+++ features/editing_tickets.feature
@@ -9,6 +9,7 @@ Feature: Editing Tickets
     And I am signed in as one of those users
     And there is a project called "TextMate 2"
     And "user@ticketee.com" can view the "TextMate 2" project
+    And "user@ticketee.com" can edit tickets for the "TextMate 2" project
     And "user@ticketee.com" has created a ticket for this project:
       | title          | description                   |
       | Make it shiny! | Gradients! Starbursts! Oh my! |
--- features/step_definitions/permission_steps.rb
+++ features/step_definitions/permission_steps.rb
@@ -6,6 +6,10 @@ Given /^"([^"]*)" can create tickets for the "([^"]*)" project$/ do |user, proje
   create_permission(user, find_project(project), "create ticket")
 end
 
+Given /^"([^"]*)" can edit tickets for the "([^"]*)" project$/ do |user, project|
+  create_permission(user, find_project(project), "edit ticket")
+end
+
 def create_permission(email, object, action)
   Permission.create!(user: User.find_by_email!(email),
                      thing: object,
