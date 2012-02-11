540080e959a30617bd16086e3e30e9d3adc8e985
Basab K. Maulik <bkm.hadoop@gmail.com>
2012-02-10 20:46:17 -0800

    Modify the CommentsController to check users for permission to modify a ticket's state. The state_id field is set to nil in the case of missing permission.

--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -1,5 +1,28 @@
 require 'spec_helper'
 
 describe CommentsController do
+  let(:user) { create_user! }
+  let(:project) { Project.create!(name: "Ticketee") }
 
+  let(:ticket) do
+    project.tickets.create(title: "State transitions",
+                           description: "Can not be hacked.",
+                           user: user)
+  end
+
+  let(:state) { State.create!(name: "New") }
+
+  context "a user without permission to change the ticket state" do
+
+    before do
+      sign_in(:user, user)
+    end
+
+    it "cannot change the ticket state by passing in a state_id" do
+      post :create, comment: { text: "Hacked!", state_id: state.id },
+        ticket_id: ticket.id
+      ticket.reload
+      ticket.state.should == nil
+    end
+  end
 end
